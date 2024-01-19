b3ff00fe3a3f699b7704e6d9a9d89d7a9262270a
Turbo Panumarch <panumarch.a@gmail.com>
2024-01-19 14:28:38 +0700

    01-04

--- 01-node-intro/exercises/01-04-more-cli-options/index.js
+++ 01-node-intro/exercises/01-04-more-cli-options/index.js
@@ -8,6 +8,18 @@ const input = process.argv[3];
 if (option === "w") {
   fs.appendFileSync(filePath, input + "\n");
   console.log("appended:", input);
+} else if (option === "r") {
+  // read
+  const content = fs.readFileSync(filePath, "utf-8");
+  console.log(content);
+} else if (option === "c") {
+  fs.truncateSync(filePath, 0);
+} else if (option === "d") {
+  // read file
+  const content = fs.readFileSync(filePath, "utf-8");
+
+  // append content to the file
+  fs.appendFileSync(filePath, "\n" + content);
 } else {
   console.log("invalid option");
 }
