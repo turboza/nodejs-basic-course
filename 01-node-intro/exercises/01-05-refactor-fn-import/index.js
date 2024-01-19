5ddff85dde9edb9d85e1f6911dee2a2280ca439c
Turbo Panumarch <panumarch.a@gmail.com>
2024-01-19 15:04:08 +0700

    01-05

--- 01-node-intro/exercises/01-05-refactor-fn-import/index.js
+++ 01-node-intro/exercises/01-05-refactor-fn-import/index.js
@@ -1,19 +1,14 @@
-import fs from "fs";
-
-const filePath = "data.txt";
+import { write, read, clear } from "./utils.js";
 
 const option = process.argv[2];
 const input = process.argv[3];
 
 if (option === "w") {
-  fs.appendFileSync(filePath, input + "\n");
-  console.log("appended:", input);
+  write(input);
 } else if (option === "r") {
-  const content = fs.readFileSync(filePath, "utf-8");
-  console.log("file content:", content);
+  read();
 } else if (option === "c") {
-  fs.writeFileSync(filePath, "");
-  console.log("the file has been clear");
+  clear();
 } else {
   console.log("invalid option");
 }
