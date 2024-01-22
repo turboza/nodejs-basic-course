e62ce2fd01f1c5c33a8003a8dc4cb32092370337
Turbo Panumarch <panumarch.a@gmail.com>
2024-01-22 10:02:06 +0700

    01-06

--- 01-node-intro/exercises/01-06-cli-coloring/utils.js
+++ 01-node-intro/exercises/01-06-cli-coloring/utils.js
@@ -1,15 +1,20 @@
 import fs from "fs";
+import clc from "cli-color"; // new
+
+// var clc = require("cli-color"); // old
 
 const filePath = "data.txt";
 
+const header = clc.blue.bgYellow.bold;
+
 export function write(input) {
   fs.appendFileSync(filePath, input + "\n");
-  console.log("appended:", input);
+  console.log("appended:", clc.red.bgWhite(input));
 }
 
 export function read() {
   const content = fs.readFileSync(filePath, "utf-8");
-  console.log("file content:");
+  console.log(header("file content:"));
   console.log(content);
 }
 
